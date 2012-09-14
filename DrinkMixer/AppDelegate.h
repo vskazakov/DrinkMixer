98196bb2c63b5d5b840f9f7d2aa7aa76f1d08053
Admin <User@Lions-Mac-2.local>
2012-09-14 21:16:11 +0300

    Initial Commit

---
+++ DrinkMixer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrinkMixer
+//
+//  Created by Lion User on 14/09/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UINavigationController *navigationController;
+
+@end
---
+++ DrinkMixer/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  DrinkMixer
+//
+//  Created by Lion User on 14/09/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (strong, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
